deff452c77900e4dd84f38f5481cd5833c83b7e0
Justin <justintay123@gmail.com>
2016-09-06 17:39:05 +0800

    Add tests for elementsAreUnique method. Also organised imports.

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -1,6 +1,10 @@
 package seedu.addressbook.common;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class UtilsTest {
@@ -23,5 +27,31 @@ public class UtilsTest {
 	Object obj2 = null;
 	final boolean result = Utils.isAnyNull(obj1, obj2);
 	assertTrue(result);
+    }
+    
+    /**
+     * Test elementsAreUnique method
+     */
+
+    @Test
+    public void elementsAreUnique_isUnique() {
+	ArrayList<Object> list = new ArrayList<Object>();
+	Object obj1 = new Object();
+	Object obj2 = new Object();
+	list.add(obj1);
+	list.add(obj2);
+	final boolean result = Utils.elementsAreUnique(list);
+	assertTrue(result);
+    }
+    
+    @Test
+    public void elementsAreUnique_notUnique() {
+	ArrayList<Object> list = new ArrayList<Object>();
+	Object obj1 = new Object();
+	Object obj2 = obj1;
+	list.add(obj1);
+	list.add(obj2);
+	final boolean result = Utils.elementsAreUnique(list);
+	assertFalse(result);
     }
 }
