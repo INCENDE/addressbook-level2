9529d169e396da90892cfe7651439a9c2dfeba17
Justin <justintay123@gmail.com>
2016-09-06 16:57:22 +0800

    Filled in the class details of Tagging - added class variables Person, Tag and isAdd. Also override the toString method.

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -1,5 +1,35 @@
 package seedu.addressbook.data.tag;
 
-public class Tagging {
+import seedu.addressbook.data.person.Person;
 
+public class Tagging {
+    private final Person person;
+    private final Tag tag;
+    private final boolean isAdd;
+
+    private static final String TAG_ADD = "+";
+    private static final String TAG_DELETE = "-";
+
+    public Tagging(Person person, Tag tag, boolean isAdd) {
+	this.person = person;
+	this.tag = tag;
+	this.isAdd = isAdd;
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder builder = new StringBuilder();
+	if (isAdd) {
+	    builder.append(TAG_ADD);
+	} else {
+	    builder.append(TAG_DELETE);
+	}
+	builder.append(" ");
+	builder.append(person.getName());
+	builder.append(" ");
+	builder.append("[");
+	builder.append(tag.tagName);
+	builder.append("]");
+	return builder.toString();
+    }
 }
