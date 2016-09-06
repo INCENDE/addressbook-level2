5abdac3ff7830260a3b577b8c3d6821880746d2f
Justin <justintay123@gmail.com>
2016-09-06 17:33:03 +0800

    Added tests for isAnyNull method

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -1,16 +1,27 @@
 package seedu.addressbook.common;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class UtilsTest {
-    
-    private Utils utils;
+
+    /**
+     * Test isAnyNull method using 2 objects
+     */
 
     @Test
-    public void test() {
-	fail("Not yet implemented");
+    public void isAnyNull_noNull() {
+	Object obj1 = new Object();
+	Object obj2 = new Object();
+	final boolean result = Utils.isAnyNull(obj1, obj2);
+	assertFalse(result);
     }
 
+    @Test
+    public void isAnyNull_hasNull() {
+	Object obj1 = new Object();
+	Object obj2 = null;
+	final boolean result = Utils.isAnyNull(obj1, obj2);
+	assertTrue(result);
+    }
 }
