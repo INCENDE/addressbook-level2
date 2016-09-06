8b11188e38beede6b04024cf9bf296d4fe34be37
Justin <justintay123@gmail.com>
2016-09-06 17:21:10 +0800

    Created class UtilsTest

---
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -0,0 +1,16 @@
+package seedu.addressbook.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UtilsTest {
+    
+    private Utils utils;
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+}
